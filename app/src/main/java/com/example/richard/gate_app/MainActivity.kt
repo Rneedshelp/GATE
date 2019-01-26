e18ad274defb4fdf5c8fa93ac19e1d93b302c969
Richard Echeverria <Rneedshelp@users.noreply.github.com>
2019-01-25 23:13:40 -0800

    testing

--- app/src/main/java/com/example/richard/gate_app/MainActivity.kt
+++ app/src/main/java/com/example/richard/gate_app/MainActivity.kt
@@ -10,15 +10,15 @@ class MainActivity : AppCompatActivity() {
     private val mOnNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
         when (item.itemId) {
             R.id.navigation_home -> {
-                message.setText(R.string.title_home)
+                message.setText("Friends")
                 return@OnNavigationItemSelectedListener true
             }
             R.id.navigation_dashboard -> {
-                message.setText(R.string.title_dashboard)
+                message.setText("Home")
                 return@OnNavigationItemSelectedListener true
             }
             R.id.navigation_notifications -> {
-                message.setText(R.string.title_notifications)
+                message.setText("Settings")
                 return@OnNavigationItemSelectedListener true
             }
         }
