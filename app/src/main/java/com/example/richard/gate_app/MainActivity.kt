9bec8a264ac8d012385710fff0ad2b72fdfa4656
Richard Echeverria <Rneedshelp@users.noreply.github.com>
2019-01-28 22:31:42 -0800

    splash page

--- app/src/main/java/com/example/richard/gate_app/MainActivity.kt
+++ app/src/main/java/com/example/richard/gate_app/MainActivity.kt
@@ -1,34 +1,14 @@
 package com.example.richard.gate_app
 
 import android.os.Bundle
-import android.support.design.widget.BottomNavigationView
 import android.support.v7.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
-    private val mOnNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
-        when (item.itemId) {
-            R.id.navigation_home -> {
-                message.setText("Friends")
-                return@OnNavigationItemSelectedListener true
-            }
-            R.id.navigation_dashboard -> {
-                message.setText("Home")
-                return@OnNavigationItemSelectedListener true
-            }
-            R.id.navigation_notifications -> {
-                message.setText("Settings")
-                return@OnNavigationItemSelectedListener true
-            }
-        }
-        false
-    }
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener)
     }
 }
---
+++ app/src/main/java/com/example/richard/gate_app/SplashActivity.kt
@@ -0,0 +1,29 @@
+package com.example.richard.gate_app
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.os.PersistableBundle
+import android.support.v7.app.AppCompatActivity
+
+
+class SplashActivity : AppCompatActivity() {
+    private var delayhandler: Handler? = null
+    private val delay : Long = 5000
+
+    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
+        super.onCreate(savedInstanceState, persistentState)
+        setContentView(R.layout.activity_splash)
+
+        delayhandler = Handler()
+
+        delayhandler?.postDelayed(runnable,delay)
+    }
+
+    internal val runnable : Runnable = Runnable{
+
+        intent = Intent(this,SplashActivity::class.java)
+        startActivity(intent)
+        finish()
+    }
+}
