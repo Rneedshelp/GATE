3f7fa8f2da763ee59ea123d308868c738a1b32ee
Richard Echeverria <Rneedshelp@users.noreply.github.com>
2019-01-30 22:10:43 -0800

    want to add animation on splash

--- app/src/main/java/com/example/richard/gate_app/SplashActivity.kt
+++ app/src/main/java/com/example/richard/gate_app/SplashActivity.kt
@@ -1,39 +1,21 @@
 package com.example.richard.gate_app
 
-import android.content.Intent
 import android.os.Bundle
 import android.os.Handler
-import android.os.PersistableBundle
 import android.support.v7.app.AppCompatActivity
+import org.jetbrains.anko.startActivity
+
 
 
 class SplashActivity : AppCompatActivity() {
-    private var delayhandler: Handler? = null
-    private val delay : Long = 10000
 
-    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
+    override fun onCreate(savedInstanceState: Bundle?) {
 
-        super.onCreate(savedInstanceState, persistentState)
         setContentView(R.layout.activity_splash)
-        val background = object : Thread() {
-            override fun run() {
-                try {
-                    // Thread will sleep for 5 seconds
-                    Thread.sleep((10 * 1000).toLong())
+        super.onCreate(savedInstanceState)
 
-                    // After 5 seconds redirect to another intent
-                    val i = Intent(baseContext, MainActivity::class.java)
-                    startActivity(i)
-                    //Remove activity
-                    finish()
-                } catch (e: Exception) {
-                    e.printStackTrace()
-                }
 
-            }
-        }
-        // start thread
-        background.start()
+        Handler().postDelayed({startActivity<MainActivity>()},3000)
+
     }
-
 }
