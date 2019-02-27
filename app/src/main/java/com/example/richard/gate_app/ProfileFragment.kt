89275ff7a7fbad001f77206f1cddbe665c7876a1
Richard Echeverria <Rneedshelp@users.noreply.github.com>
2019-02-26 23:05:58 -0800

    databinding on userprofile and friends

--- app/src/main/java/com/example/richard/gate_app/ProfileFragment.kt
+++ app/src/main/java/com/example/richard/gate_app/ProfileFragment.kt
@@ -6,15 +6,26 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
-import androidx.databinding.DataBindingUtil.inflate
-import androidx.databinding.ViewDataBinding
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProviders
+import com.example.richard.gate_app.databinding.FragmentProfileBinding
+import com.example.richard.gate_app.databinding.FragmentProfileBindingImpl
 
 
 class ProfileFragment : Fragment() {
 
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
 
+        val bind = DataBindingUtil.inflate<FragmentProfileBinding>(
+            inflater,
+            R.layout.fragment_profile,
+            container,
+            false
+        )
+        //assigning the ViewModel object to a property in the binding class to help use binding adapters in XML
+        bind.user = ViewModelProviders.of(this).get(ViewModelClass::class.java)
+        return bind.root
+    }
 
 
 }
